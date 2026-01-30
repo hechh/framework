41f6278f7ca3a448cdc7e75e7cf351b79f266bfd
unknown <812417032@qq.com>
2026-01-30 17:47:07 +0800

    保存代码

--- cluster/internal/entity/cluster.go
+++ cluster/internal/entity/cluster.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/hechh/framework"
 	"github.com/hechh/framework/packet"
-	"github.com/hechh/library/pool"
+	"github.com/hechh/library/mlog"
 	"github.com/hechh/library/random"
 )
 
@@ -14,7 +14,7 @@ type Cluster struct {
 	mutex    sync.RWMutex
 	nodeType uint32
 	data     map[uint32]*packet.Node
-	buckets  [framework.CLUSTER_BUCKET_SIZE]*packet.Node
+	buckets  [framework.CLUSTER_BUCKET_SIZE]uint32
 }
 
 func NewCluster(nodeType uint32) framework.ICluster {
@@ -46,15 +46,16 @@ func (c *Cluster) Add(node *packet.Node) {
 	per := int(math.Ceil(framework.CLUSTER_BUCKET_SIZE / float64(len(c.data))))
 	tmps := map[uint32]int{}
 	for pos := 0; pos < framework.CLUSTER_BUCKET_SIZE; pos++ {
-		item := c.buckets[pos]
-		if item == nil || tmps[item.Id] >= per {
-			item = node
+		nodeId := c.buckets[pos]
+		if nodeId <= 0 || tmps[nodeId] >= per {
+			nodeId = node.Id
 		}
-		if tmps[item.Id] < per {
-			tmps[item.Id]++
-			c.buckets[pos] = node
+		if tmps[nodeId] < per {
+			tmps[nodeId]++
+			c.buckets[pos] = nodeId
 		}
 	}
+	mlog.Tracef("[cluster] 添加节点%v", c.buckets)
 }
 
 // 删除节点
@@ -65,15 +66,15 @@ func (c *Cluster) Del(nodeId uint32) (nn *packet.Node) {
 	delete(c.data, nodeId)
 	if len(c.data) <= 0 {
 		for pos := 0; pos < framework.CLUSTER_BUCKET_SIZE; pos++ {
-			c.buckets[pos] = nil
+			c.buckets[pos] = 0
 		}
 		return
 	}
 
 	tmps := map[uint32]int{}
 	for pos := 0; pos < framework.CLUSTER_BUCKET_SIZE; pos++ {
-		if item := c.buckets[pos]; item.Id != nodeId {
-			tmps[item.Id]++
+		if nodeId := c.buckets[pos]; nodeId != nodeId {
+			tmps[nodeId]++
 		}
 	}
 
@@ -81,15 +82,16 @@ func (c *Cluster) Del(nodeId uint32) (nn *packet.Node) {
 	per := int(math.Ceil(framework.CLUSTER_BUCKET_SIZE / float64(len(c.data))))
 	for _, node := range c.data {
 		for ; pos < framework.CLUSTER_BUCKET_SIZE; pos++ {
-			if item := c.buckets[pos]; item.Id == nodeId {
+			if nodeId := c.buckets[pos]; nodeId == nodeId {
 				if tmps[node.Id] >= per {
 					break
 				}
 				tmps[node.Id]++
-				c.buckets[pos] = node
+				c.buckets[pos] = nodeId
 			}
 		}
 	}
+	mlog.Tracef("[cluster] 删除节点：%v", c.buckets)
 	return
 }
 
@@ -97,21 +99,34 @@ func (c *Cluster) Random(seed uint64) *packet.Node {
 	c.mutex.RLock()
 	defer c.mutex.RUnlock()
 	if seed <= 0 {
-		return c.buckets[random.Intn(framework.CLUSTER_BUCKET_SIZE)]
+		if nodeId := c.buckets[random.Intn(framework.CLUSTER_BUCKET_SIZE)]; nodeId > 0 {
+			return c.data[nodeId]
+		}
+		return nil
 	}
-	// hash路由
-	h := pool.GetHash64()
-	defer pool.PutHash64(h)
-	h.Reset()
-	var b [8]byte
-	b[0] = byte(seed >> 56)
-	b[1] = byte(seed >> 48)
-	b[2] = byte(seed >> 40)
-	b[3] = byte(seed >> 32)
-	b[4] = byte(seed >> 24)
-	b[5] = byte(seed >> 16)
-	b[6] = byte(seed >> 8)
-	b[7] = byte(seed)
-	h.Write(b[:])
-	return c.buckets[h.Sum64()%framework.CLUSTER_BUCKET_SIZE]
+	if nodeId := c.buckets[seed%framework.CLUSTER_BUCKET_SIZE]; nodeId > 0 {
+		return c.data[nodeId]
+	}
+	return nil
+	/*
+		// hash路由
+		h := pool.GetHash64()
+		defer pool.PutHash64(h)
+		h.Reset()
+		var b [8]byte
+		b[0] = byte(seed >> 56)
+		b[1] = byte(seed >> 48)
+		b[2] = byte(seed >> 40)
+		b[3] = byte(seed >> 32)
+		b[4] = byte(seed >> 24)
+		b[5] = byte(seed >> 16)
+		b[6] = byte(seed >> 8)
+		b[7] = byte(seed)
+		h.Write(b[:])
+		nodeId := c.buckets[h.Sum64()%framework.CLUSTER_BUCKET_SIZE]
+		if nodeId > 0 {
+			return c.data[nodeId]
+		}
+		return nil
+	*/
 }
